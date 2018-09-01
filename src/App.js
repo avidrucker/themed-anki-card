5e30877f77ecb3ddf2a001588403f9d6ba76c2c4
Avi Drucker <avidrucker@gmail.com>
2018-08-31 20:38:57 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+        <header className="bg-blue white">
+          <h1 className="ma0 pa3">Welcome to React</h1>
+        </header>
+        <p className="pa3">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
